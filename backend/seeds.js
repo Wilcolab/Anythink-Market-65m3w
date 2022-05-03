04f4aa23abec8cec2132ce713f8b1b4c545c9957
Gavriel Cohen <gavriel@Gavriels-MacBook-Pro.local>
2022-05-04 00:53:06 +0300

    adds dataseeding script

---
+++ backend/seeds.js
@@ -0,0 +1,29 @@
+var mongoose = require("mongoose");
+require("./models/User");
+require("./models/Item");
+require("./models/Comment");
+require("./config/passport");
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.set("debug", true);
+var Item = mongoose.model("Item");
+var User = mongoose.model("User");
+
+var addItems = async () => {
+    const id = "USER_ID";
+    const user = await User.findById(id);
+    for (var i = 0; i<50; i++) {
+        let item = new Item({
+            title: `random ${Math.ceil(Math.random() * 100)}`,
+            description: `random ${Math.ceil(Math.random() * 100)}`,
+            image: `https://picsum.photos/id/${Math.ceil(Math.random() * 100)}/200/300`,
+            tagList: '',
+          });
+    
+        item.seller = user;
+    
+        await item.save();
+    }
+
+}
+
+addItems().then(process.exit);
